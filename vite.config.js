96f9b150d58e46cbeff48a8145f65b286f8a254e
Aiden <jason_dlb@sina.cn>
2022-06-30 10:23:13 +0800

    project init

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: Aiden(戴林波)
+ * @Date: 2022-06-29 17:47:58
+ * @LastEditTime: 2022-06-29 17:51:50
+ * @LastEditors: Aiden(戴林波)
+ * @Description: 
+ * @Email: jason_dlb@sina.cn
+ */
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()]
+})
